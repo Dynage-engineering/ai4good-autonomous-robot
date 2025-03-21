82ae4e44f4248b6d0b5f514a6889813f7cb27478
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-03-21 12:37:45 +0100

    update pin configuration for DC motor, ultrasonic sensor, color sensor, gripper, and steering servo

--- include/pin_config.h
+++ include/pin_config.h
@@ -2,25 +2,25 @@
 #define PIN_CONFIG_H
 
 // Pin declarations for each component
-#define DC_MOTOR_PIN1 9
-#define DC_MOTOR_PIN2 8
-#define DC_MOTOR_ENABLE 7
+#define DC_MOTOR_PIN1 22
+#define DC_MOTOR_PIN2 23
+#define DC_MOTOR_ENABLE 21
 
-#define ULTRASONIC_TRIGGER_PIN 5
+#define ULTRASONIC_TRIGGER_PIN 13
 #define ULTRASONIC_ECHO_PIN 6
 
-#define COLOR_SENSOR_S0 7
-#define COLOR_SENSOR_S1 8
-#define COLOR_SENSOR_S2 9
-#define COLOR_SENSOR_S3 10
-#define COLOR_SENSOR_OUT 11
+#define COLOR_SENSOR_S0 18
+#define COLOR_SENSOR_S1 5
+#define COLOR_SENSOR_S2 17
+#define COLOR_SENSOR_S3 4
+#define COLOR_SENSOR_OUT 16
 
-#define GRIPPER_PICKER 12
-#define GRIPPER_ELBOW 13
-#define GRIPPER_SHOULDER 14
-#define GRIPPER_WAIST 15
+#define GRIPPER_PICKER 26
+#define GRIPPER_ELBOW 25
+#define GRIPPER_SHOULDER 33
+#define GRIPPER_WAIST 32
 
-#define STEERING_SERVO_PIN 16
+#define STEERING_SERVO_PIN 27
 
 void initializePins();
 
